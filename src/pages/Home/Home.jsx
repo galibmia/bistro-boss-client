eff7527fad2ce165b49e2b3ae2febc02e9f7e866
Galib Mia <gm71bd@gmail.com>
2024-08-13 11:54:51 +0600

    Service section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Category from './Category/Category';
+import Service from './Service/Service';
 
 const Home = () => {
     return (
@@ -10,6 +11,9 @@ const Home = () => {
             </div>
             <div className='my-20'>
                 <Category></Category>
+            </div>
+            <div className='my-20'>
+                <Service></Service>
             </div>
         </div>
     );
