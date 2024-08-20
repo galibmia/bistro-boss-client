31cc0564ed0628962adc1136502cfb496a7eca89
Galib Mia <gm71bd@gmail.com>
2024-08-20 21:43:22 +0600

    Add change the role of the users and delete the users.

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -3,6 +3,8 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { Helmet } from 'react-helmet-async';
 import { useQuery } from '@tanstack/react-query';
 import { RiDeleteBin6Line } from 'react-icons/ri';
+import { FaUserShield } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const { data: users = [], refetch } = useQuery({
@@ -14,7 +16,65 @@ const AllUsers = () => {
     });
 
     const handleDeleteUser = (id) => {
-        // Logic for deleting a user
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to delete this user?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`http://localhost:5000/users/${id}`, {
+                        method: 'DELETE'
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.deletedCount > 0) {
+                                refetch();
+                                Swal.fire({
+                                    title: "Deleted!",
+                                    text: "User has been deleted.",
+                                    icon: "success"
+                                });
+                            }
+                        })
+                }
+            });
+    }
+
+
+// Handle function to change the role 
+    const handleChangeRole = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to change the role?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Make Admin"
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`http://localhost:5000/users/admin/${id}`, {
+                        method: 'PATCH'
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.modifiedCount > 0) {
+                                refetch();
+                                Swal.fire({
+                                    title: "Updated!",
+                                    text: "User role has been updated.",
+                                    icon: "success"
+                                });
+                        }
+                    })
+                }
+            });
     }
 
     return (
@@ -51,7 +111,7 @@ const AllUsers = () => {
                                             <td className='font-bold text-xl'>{index + 1}</td>
                                             <td>{user.name}</td>
                                             <td><div className="text-[#737373]">{user.email}</div></td>
-                                            <td className="text-[#737373]">{user.role}</td>
+                                            <td className="text-2xl">{user.role === 'admin' ? 'Admin' : <button onClick={() => handleChangeRole(user._id)} className='bg-[#D1A054] w-[50px] py-3 mx-auto rounded-lg text-white'><FaUserShield className='mx-auto' /></button>}</td>
                                             <td>
                                                 <button onClick={() => handleDeleteUser(user._id)} className='bg-red-600 text-white p-3 text-2xl rounded-lg'>
                                                     <RiDeleteBin6Line />
